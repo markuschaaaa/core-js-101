026e9baaa305ac04c2ad2c6a023a90d91af38d18
markuschaaaa <chumakoff03@gmail.com>
2023-01-02 15:27:50 +0300

    implement few tasks

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return new Date(value);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value);
 }
 
 
@@ -53,8 +53,12 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const fullYear = new Date(date).getFullYear();
+  if (fullYear % 4 === 0 && (!(fullYear % 400) || fullYear % 100)) {
+    return true;
+  }
+  return false;
 }
 
 
@@ -73,8 +77,25 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const newEndDate = new Date(endDate);
+  const newStartDate = new Date(startDate);
+
+  const milliseconds = (
+    newEndDate.getMilliseconds() - newStartDate.getMilliseconds())
+    .toString()
+    .padStart(3, 0);
+  const seconds = (newEndDate.getSeconds() - newStartDate.getSeconds())
+    .toString()
+    .padStart(2, 0);
+  const minutes = (newEndDate.getMinutes() - newStartDate.getMinutes())
+    .toString()
+    .padStart(2, 0);
+  const hours = (newEndDate.getHours() - newStartDate.getHours())
+    .toString()
+    .padStart(2, 0);
+
+  return `${hours}:${minutes}:${seconds}.${milliseconds}`;
 }
 
 
